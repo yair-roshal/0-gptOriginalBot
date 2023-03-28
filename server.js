a57a02e4528ae6f436917e1173707b808b15a566
yair-roshal <yair7705778@gmail.com>
2023-03-28 13:24:31 +0300

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api')
 const token = process.env.TELEGRAM_BOT_TOKEN
 console.log('process.env :>> ', process.env)
-console.log('token :>> ', token)
+console.log('token222 :>> ', token)
 const bot = new TelegramBot(token, { polling: true })
 const chatIdAdmin = process.env.CHAT_ID_ADMIN
 
