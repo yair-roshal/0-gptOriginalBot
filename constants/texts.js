5699d0a2e4cba34e9c5acfbf0a560258b0237fe7
yair-roshal <yair7705778@gmail.com>
2023-03-31 11:55:22 +0300

    fix

--- constants/texts.js
+++ constants/texts.js
@@ -8,11 +8,20 @@ This bot gives you access to OpenAI products such as ChatGPT and Dall-E to creat
 
 ⚡Bot uses the same model as ChatGPT: gpt-3.5-turbo.
 
-The chat bot is able to:
-1. Write and edit texts
-2. Translate from any language to any language
-3. Write and edit code
-Answering questions
+A small example of what this bot can do:
+-Write and edit texts
+-Translate from any language to any language
+-Write and edit code
+
+-Code, teach coding, look for errors in code 
+-Create websites, apps, mini-games.
+-Fills Excel spreadsheets for you, makes presentations in PowerPoint
+-Write essays/stories/songs/poems/articles on any topic
+-Answers difficult questions.
+-Generates queries for other neural networks
+
+And much more! The possibilities of the bot are limited only by your imagination. Use it and share it with your friends! 👉🏻 <a href="${link_to_bot}">GPT Original Bot</a>
+
 
 You can communicate with the bot as with a live interlocutor by asking questions in any language. Note that sometimes the bot makes up facts and has limited knowledge about events after 2021.
 
@@ -22,6 +31,18 @@ You can communicate with the bot as with a live interlocutor by asking questions
 
 💬 If you have any questions or suggestions, email admin @yair770.
 
+
+Tips for proper use:
+- Ask meaningful questions, write in more detail.
+- Don't write nonsense or you'll get the same nonsense in return.
+
+Examples of questions/questions:
+~ What is 7 * 8?
+~ When did World War II start?
+~ Write Python calculator code.
+~ Write an essay about how I spent my summer
+
+🔥 Чтобы начать общение, напиши что-нибудь CHATGPT в строку ниже 👇🏻
 
 <a href="${link_to_bot}">GPT Original Bot</a> | <a href="${link_to_public}">News AI</a>
  `
