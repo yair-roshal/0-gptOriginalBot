2ba940f10259b224b19cac13a6447af04b0cdaba
yair-roshal <yair7705778@gmail.com>
2023-04-02 02:50:26 +0300

    MVP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,24 +1,25 @@
 module.exports = {
     apps: [
         {
-            name: 'PROJECT_NAME',
-            script: './index.js',
-            instances: 2,
+            name: 'newBot',
+            script: './bot.js',
+            instances: 1,
             max_memory_restart: '300M',
             // Logging
-            out_file: './out.log',
-            error_file: './error.log',
+            out_file: './newBot-out.log',
+            error_file: './newBot-error.log',
             merge_logs: true,
             log_date_format: 'DD-MM HH:mm:ss Z',
             log_type: 'json',
             // Env Specific Config
             env_production: {
-                NODE_ENV: 'production',
+                NODE_ENV: 'prod',
                 PORT: 8080,
                 exec_mode: 'cluster_mode',
+                watch: true,
             },
             env_development: {
-                NODE_ENV: 'development',
+                NODE_ENV: 'dev',
                 PORT: 8080,
                 watch: true,
                 watch_delay: 3000,
