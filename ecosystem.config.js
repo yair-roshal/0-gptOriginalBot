4ef6bae4daf2756d2e70c3e59b07d784fd6b1e5b
yair-roshal <yair7705778@gmail.com>
2023-04-27 14:31:32 +0300

    MVP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'newBot',
-            script: './bot.js',
+            script: './bot/bot.js',
             max_memory_restart: '300M',
             // Logging
             out_file: './newBot-out.log',
